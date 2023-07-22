c0276c63616dbfed9e598923e638c36703011909
antonfrancisjeejo <antonfrancisjeejo13@gmail.com>
2023-07-22 22:59:49 +0530

    Initial commit

---
+++ app/about/team/page.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Cursus in hac
+        habitasse platea dictumst quisque sagittis purus sit. Arcu risus quis
+        varius quam. Turpis egestas pretium aenean pharetra magna ac. Ipsum
+        dolor sit amet consectetur adipiscing elit. Nibh venenatis cras sed
+        felis. Sed cras ornare arcu dui vivamus arcu felis bibendum.
+        Pellentesque diam volutpat commodo sed egestas egestas fringilla. In
+        nibh mauris cursus mattis molestie a. Consequat semper viverra nam
+        libero justo laoreet sit. Aliquet lectus proin nibh nisl condimentum.
+        Tortor dignissim convallis aenean et. Eleifend quam adipiscing vitae
+        proin sagittis nisl rhoncus mattis rhoncus. Venenatis cras sed felis
+        eget. Scelerisque viverra mauris in aliquam sem fringilla ut morbi
+        tincidu
+      </p>
+    </div>
+  );
+};
+
+export default TeamPage;
